5fcbad64e4ddb93e4225491f8191704183b77633
Rodrigo Montiel <rodrigo.montiel@correo.ucu.edu.uy>
2025-09-22 00:50:39 -0300

    fin de los tests

--- test/LibraryTests/Elfo_tests.cs
+++ test/LibraryTests/Elfo_tests.cs
@@ -1,6 +1,25 @@
-namespace LibraryTests;
+using Library;
 
-public class Elfo_tests
+namespace ProgramNamespace 
 {
-    
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Elfo Patri = new Elfo("Patri");
+            Elfo Rodri = new Elfo("Rodri");
+
+            
+            Item arco = new Item("arco", 20, 0);
+            Item escudo = new Item("escudo", 0, 20);
+
+            Patri.AgregarItem(arco);
+            Rodri.AgregarItem(escudo);
+
+            // Probar ataque
+            Patri.Atacar_Elfo(Rodri);
+            Console.WriteLine($"Vida de {Rodri.Nombre} después del ataque: {Rodri.Vida}");
+        }
+    }
 }
