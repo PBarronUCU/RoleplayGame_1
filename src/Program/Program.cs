97b5a815858402d1f6e8422b7ad855c1e76b5ffc
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-09-22 00:13:36 -0300

    Agrego algo en program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -13,6 +13,11 @@ public static class Program
         Item Cimitarra = new Item("cimitarra", 110, 0);
         Item Escudo = new Item("escudo", 0, 20);
         ItemMagico MoonlightGreatsword = new ItemMagico("MoonlightGreatsword",50,0);
+        Patri.Agregar_ItemMagico(MoonlightGreatsword);
+        Patri.Agregar_Libro(Necronomicon);
+        Rodri.AgregarItem(Cimitarra);
+        Rodri.AgregarItem(Escudo);
+        Necronomicon.Agregar(Fireball);
         Patri.Atacar_Elfo(Rodri);
         Console.WriteLine($"{Patri.Nombre} ataco a {Rodri.Nombre}.{Rodri.Nombre} quedo con {Rodri.Vida}");
         Rodri.Atacar_Mago(Patri);
