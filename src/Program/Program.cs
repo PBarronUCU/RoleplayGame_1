312b73edcf4727bdcdb1662d87c2a727a8aaf3a6
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-09-21 22:26:14 -0300

    Agrego algunas operacinoes

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Library;
+
+namespace Program;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Mago Patri = new Mago("Patri");
+        Elfo Rodri = new Elfo("Rodri");
+        Libro Necronomicon =  new Libro("Necronomicon");
+        Hechizo Fireball = new Hechizo("Fireball",10);
+        Item Cimitarra = new Item("cimitarra", 110, 0);
+        Item Escudo = new Item("escudo", 0, 20);
+        ItemMagico MoonlightGreatsword = new ItemMagico("MoonlightGreatsword",50,0);
+        Patri.Atacar_Elfo(Rodri);
+        Console.WriteLine($"{Patri.Nombre} ataco a {Rodri.Nombre}.{Rodri.Nombre} quedo con {Rodri.Vida}");
+        Rodri.Atacar_Mago(Patri);
+        Console.WriteLine($"{Rodri.Nombre} ataco a {Patri.Nombre}.{Patri.Nombre} quedo con {Patri.Vida}");
+    }
+}
